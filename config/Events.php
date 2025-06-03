44e7d73d578084cc77291f13d23490415519dc24
Alexei Karpenko <karpen_ko@hotmail.com>
2025-06-03 21:15:59 +0200

    added event for getting projektbeurteilung document download link

---
+++ config/Events.php
@@ -0,0 +1,29 @@
+<?php
+if (! defined('BASEPATH')) exit('No direct script access allowed');
+
+use CI3_Events as Events;
+use FHCAPI_Controller as FHCAPI_Controller;
+
+Events::on('projektbeurteilung_download_link', function ($projektarbeit_id, $betreuerart_kurzbz, $person_id, $downloadLinkFunc) {
+
+	$ci =& get_instance();
+
+	$ci->load->model('extensions/FHC-Core-Projektarbeitsbeurteilung/Projektarbeitsbeurteilung_model', 'ProjektarbeitsbeurteilungModel');
+
+	$ci->ProjektarbeitsbeurteilungModel->addSelect('1');
+	$ci->ProjektarbeitsbeurteilungModel->db->where('abgeschicktvon IS NOT NULL', NULL, FALSE);
+	$result = $ci->ProjektarbeitsbeurteilungModel->loadWhere(
+		[
+			'projektarbeit_id' => $projektarbeit_id,
+			'betreuerart_kurzbz' => $betreuerart_kurzbz,
+			'betreuer_person_id' => $person_id
+		]
+	);
+
+	$downloadLink = hasData($result)
+		? APP_ROOT.'addons/fhtw/content/projektbeurteilung/projektbeurteilungDocumentExport.php'.
+			'?projektarbeit_id='.$projektarbeit_id.'&betreuerart_kurzbz='.$betreuerart_kurzbz.'&person_id='.$person_id
+		: null;
+
+	$downloadLinkFunc($downloadLink);
+});
